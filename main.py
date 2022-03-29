5aeeefb3e722090cd041d03b0ec7a929f064746d
Thomas Woltjer <Thomas.Woltjer@VibrationResearch.com>
2022-03-28 23:45:22 -0400

    Update 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, make_response
 from lorem import paragraph
 
 app = Flask(__name__)
@@ -19,18 +19,89 @@ def login():
     if request.method == 'GET':
         if session['loggedIn']:
             return redirect("/")
+        else:
+            return render_template('login.html',
+                                   nav_links=get_nav_links(),
+                                   display_mode=display_mode,
+                                   error='')
+    elif request.method == 'POST':
+        form_data = request.form
+        user = ""
+        password = ""
+        for key, value in form_data.items():
+            if key == "user":
+                user = value
+            if key == "pass":
+                password = value
+        if authenticate(user, password):
+            session['loggedIn'] = True
+            return redirect('/preferences/')
+        else:
+            return render_template('login.html',
+                                   nav_links=get_nav_links(),
+                                   display_mode=display_mode,
+                                   error='Authentication failed. Please try again.')
+    else:
+        raise NotImplementedError('Request method not implemented: ' + request.method)
 
 
+@app.route('/logout/')
+def logout():
+    display_mode = cookie_update(request.cookies)
+    if session['loggedIn']:
+        session['loggedIn'] = False
+        return render_template('logout.html',
+                               nav_links=get_nav_links(),
+                               display_mode=display_mode)
+    else:
+        return render_template('error.html',
+                               nav_links=get_nav_links(),
+                               display_mode=display_mode,
+                               error='You are not logged in')
 
 
+@app.route('/preferences/', methods=['POST', 'GET'])
+def preferences():
+    set_display_mode = ''
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'Light Mode':
+            set_display_mode = 'light'
+        elif request.form['submit_button'] == 'Dark Mode':
+            set_display_mode = 'dark'
+        else:
+            pass  # unknown
+    display_mode = cookie_update(request.cookies, set_display_mode)
+    if session['loggedIn']:
+        res = make_response(
+            render_template('preferences.html',
+                            nav_links=get_nav_links(),
+                            display_mode=display_mode))
+        if set_display_mode != '':
+            res.set_cookie('displayMode', set_display_mode, max_age=60 * 60 * 24 * 3)
+        return res
+    else:
+        return render_template('error.html',
+                               nav_links=get_nav_links(),
+                               display_mode=display_mode,
+                               error='You must be logged in to view this page.')
+
+
+def authenticate(user, password):
+    return user == 'CIS658' and password == 'WebArchitectures'
 
 
 def get_nav_links():
-    return {
-        "Home": "/",
-        # "Gallery": "/gallery",
-        # "Contact Us": "/contact"
-    }
+    if session['loggedIn']:
+        return {
+            "Home": "/",
+            "Log Out": "/logout",
+            "Preferences": "/preferences"
+        }
+    else:
+        return {
+            "Home": "/",
+            "Login": "/login"
+        }
 
 
 def get_articles():
@@ -41,9 +112,11 @@ def get_articles():
     }
 
 
-def cookie_update(cookies):
+def cookie_update(cookies, display_override=''):
     if 'loggedIn' not in session:
         session['loggedIn'] = False
+    if display_override != '':
+        return display_override
     display_mode = 'light'
     if cookies.get('displayMode') == 'dark':
         display_mode = 'dark'
