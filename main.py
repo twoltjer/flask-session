02c63c7d2c4b107357eee3c7dca2898eeb8f8a78
Thomas Woltjer <Thomas.Woltjer@VibrationResearch.com>
2022-03-28 22:21:01 -0400

    Update

--- main.py
+++ main.py
@@ -1,23 +1,55 @@
-from flask import Flask, render_template, request
-import markdown
-from datetime import datetime
+from flask import Flask, render_template, request, session, redirect
+from lorem import paragraph
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=['POST', 'GET'])
-def handle_request():
+@app.route("/")
+def homepage():
+    display_mode = cookie_update(request.cookies)
+    return render_template('homepage.html',
+                           nav_links=get_nav_links(),
+                           display_mode=display_mode,
+                           articles=get_articles())
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    display_mode = cookie_update(request.cookies)
     if request.method == 'GET':
-        return render_template('main.html', md_html="", md_raw="##Click \"Submit\" button below to render this!")
-    if request.method == 'POST':
-        form_data = request.form
-        md_input = ""
-        for key, value in form_data.items():
-            if key == "md_input":
-                md_input = value
-        html = markdown.markdown(md_input)
-        return render_template('main.html', md_html=html, md_raw=md_input)
+        if session['loggedIn']:
+            return redirect("/")
+
+
+
+
+
+
+def get_nav_links():
+    return {
+        "Home": "/",
+        # "Gallery": "/gallery",
+        # "Contact Us": "/contact"
+    }
+
+
+def get_articles():
+    return {
+        "Blog post 1": paragraph(),
+        "Blog post 2": paragraph(),
+        "Blog post 3": paragraph(),
+    }
+
+
+def cookie_update(cookies):
+    if 'loggedIn' not in session:
+        session['loggedIn'] = False
+    display_mode = 'light'
+    if cookies.get('displayMode') == 'dark':
+        display_mode = 'dark'
+    return display_mode
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.secret_key = "ThisIsAVerySecretSecretKey"
+    app.run(host='0.0.0.0', port=8000)
